2e135db6d6dd5fc3790d347c439c184978dc5548
luvoldfashioned <storm6781@naver.com>
2025-09-11 15:38:23 +0900

    11653 다시 풀어봄

--- 11653re.py
+++ 11653re.py
@@ -1,15 +1,3 @@
-# # GG
-# num = int(input())
-# i = 2
-# while num > 1:
-#     if num % i == 0:
-#         num = num // i
-#         print(i)
-#     else:
-#         i += 1
-
-
-# 다시 짠 코드
 num = int(input())
 i = 2
 while i <= num:
